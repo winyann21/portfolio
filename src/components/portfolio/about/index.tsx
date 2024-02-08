3f638e23c21691f09595564ba2147a7d89597d52
Sherwin Romero <it.sherwinromero@gmail.com>
2024-02-08 16:28:24 +0800

    done with techstack section and updated sizes of svgs

--- src/components/portfolio/about/index.tsx
+++ src/components/portfolio/about/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, OverlayTrigger, Tooltip } from "react-bootstrap";
 import "./styles.scss";
 import techStacks from "../../../resources/tech-stacks/tech-stacks.json";
 import { ReactSVG } from "react-svg";
@@ -10,21 +10,29 @@ interface TechStackInterface {
 
 const AboutPage = () => {
   return (
-    <section className="about-section mb-5 pt-3">
+    <section className="about-section mb-5 py-5">
       <Container>
-        <div className="tech-stack-title w-100 text-center mb-3">
-          <h2>Tech Stack</h2>
-          <p>Technologies I've used</p>
+        <div className="tech-stack-title w-100 text-center mb-5">
+          <h2>My Tech Stack</h2>
+          <p>Explore My Tech Universe.</p>
         </div>
         <div className="tech-stacks">
           {techStacks.map((techStack: TechStackInterface, idx) => {
             const techStackIcon = `/src/assets/svgs/${techStack.icon}`;
 
             return (
-              <div key={idx} className="text-center">
-                <Card style={{ width: "20%" }}>
-                  <ReactSVG src={techStackIcon} />
-                </Card>
+              // <OverlayTrigger
+              //   key={idx}
+              //   overlay={<Tooltip placement="top">{techStack.name}</Tooltip>}
+              // >
+              //   <div className="tech-stack-item">
+              //     <ReactSVG src={techStackIcon} />
+              //   </div>
+              // </OverlayTrigger>
+
+              <div key={idx} className="tech-stack-item">
+                <ReactSVG src={techStackIcon} />
+                <p className="mt-5">{techStack.name}</p>
               </div>
             );
           })}
